5e079538576d1d394d08e63e372a11e01829cb7f
Melvin Rivas <117697077+andres95monsalve@users.noreply.github.com>
2023-10-13 03:39:26 -0400

    commit

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-     document.addEventListener('DOMContentLoaded', function() {
-            // Obtiene todas las imágenes pequeñas de la lista
-            const smallImages = document.querySelectorAll('.image-item img');
+document.addEventListener('DOMContentLoaded', function() {
+    // Obtiene todas las imágenes pequeñas de la lista
+    const smallImages = document.querySelectorAll('.image-item img');
 
-            // Obtiene la imagen principal
-            const mainImage = document.querySelector('.principal-bike');
+    // Obtiene la imagen principal
+    const mainImage = document.querySelector('.principal-bike');
 
-            // Agrega un evento clic a cada imagen pequeña
-            smallImages.forEach(function(smallImage) {
-                smallImage.addEventListener('click', function() {
-                    // Obtiene la ruta de la imagen clickeada
-                    const newImageSrc = smallImage.getAttribute('src');
+    // Agrega un evento clic a cada imagen pequeña
+    smallImages.forEach(function(smallImage) {
+        smallImage.addEventListener('click', function() {
+            // Obtiene la ruta de la imagen clickeada
+            const newImageSrc = smallImage.getAttribute('src');
 
-                    // Cambia la imagen principal con la imagen clickeada usando GSAP
-                    gsap.to(mainImage, { src: newImageSrc, duration: 0.5 });
-                });
-            });
+            // Cambia la imagen principal con la imagen clickeada
+            mainImage.setAttribute('src', newImageSrc);
         });
+    });
+});
